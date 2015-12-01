4d188d02ae39e5848704e0c27193bdafd60afe3c
Jackson Bailey <Jackson.Bailey@equifax.com>
2015-12-01 16:45:44 -0500

    Refactor into executable & deployable war files
    Spring Boot embeds tomcat into the jar file, using this method we can
    make war files that can be deployed to a tomcat server but still be run
    with `java -jar foo.war`.

--- config-server/src/main/java/ignoreme/learn_spring_cloud/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/ignoreme/learn_spring_cloud/configserver/ConfigServerApplication.java
@@ -2,13 +2,41 @@ package ignoreme.learn_spring_cloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConfigServerApplication.class);
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean servletBean = new ServletRegistrationBean(dispatcherServlet());
+        servletBean.setName("appServlet");
+        servletBean.setLoadOnStartup(1);
+        servletBean.addInitParameter("contextConfig",
+                "org.springframework.web.context.support.AnnotationConfigWebApplicationContext");
+        servletBean.addInitParameter("contextConfigLocation",
+                "ignoreme.learn_spring_cloud.greeter.ConfigServerApplication");
+        return servletBean;
+    }
+
 }
--- greeter/src/main/java/ignoreme/learn_spring_cloud/greeter/GreeterApplication.java
+++ greeter/src/main/java/ignoreme/learn_spring_cloud/greeter/GreeterApplication.java
@@ -2,12 +2,38 @@ package ignoreme.learn_spring_cloud.greeter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
-public class GreeterApplication {
+public class GreeterApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GreeterApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GreeterApplication.class);
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean servletBean = new ServletRegistrationBean(dispatcherServlet());
+        servletBean.setName("appServlet");
+        servletBean.setLoadOnStartup(1);
+        servletBean.addInitParameter("contextConfig",
+                "org.springframework.web.context.support.AnnotationConfigWebApplicationContext");
+        servletBean.addInitParameter("contextConfigLocation", "ignoreme.learn_spring_cloud.greeter.GreeterApplication");
+        return servletBean;
     }
 
 }
